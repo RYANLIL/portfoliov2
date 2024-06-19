e06294df11e6333bf3eb9055923a4fe87054de98
RyanL <RYANLIL@github.com>
2024-06-19 16:59:05 -0400

    (wip) Add spotlight from aceternity

---
+++ components/Hero.tsx
@@ -0,0 +1,29 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Spotlight } from "./ui/Spotlight";
+
+function Hero() {
+  return (
+    <div className="pb-20 pt-36">
+      <div>
+        <Spotlight
+          className="-top-40 -left-10 md:-left32 md:top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="h-[80vh] w-[50vw] top-10 left-full"
+          fill="purple"
+        />
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+      </div>
+    </div>
+  );
+}
+
+Hero.propTypes = {};
+
+export default Hero;
